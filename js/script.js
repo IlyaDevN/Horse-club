d26da2560190f64d4ce70026d32fd52e548c406a
IlyaDevN <nevedrov.87@gmail.com>
2022-09-30 23:22:41 +0300

    slider adapted to 1920px

---
+++ js/script.js
@@ -0,0 +1,31 @@
+'use strict'
+
+ourVisitors__sliderCheckbox.addEventListener("click", function(event){
+	if(!event.target.closest("button") && event.target.tagName != "INPUT") return;
+	let photoContainerCoords = ourVisitors__photo_container.getBoundingClientRect();
+	let btn = event.target.closest("button");
+	
+	if(btn && btn.classList.contains("btn_prev")){
+		slider_range_ourVis.value--;
+		if(slider_range_ourVis.value <= 1) {
+			slider_range_ourVis.value = 1;
+		}
+	}
+	if(btn && btn.classList.contains("btn_next")){
+		slider_range_ourVis.value++;
+		if(slider_range_ourVis.value >= 3) {
+			slider_range_ourVis.value = 3;
+		}
+	}
+	switch (+slider_range_ourVis.value) {
+		case 1:
+			ourVisitors__photo_container.style.marginLeft = "0px";
+			break;
+		case 2:
+			ourVisitors__photo_container.style.marginLeft = -photoContainerCoords.width + "px";
+			break;
+		case 3:
+			ourVisitors__photo_container.style.marginLeft = -photoContainerCoords.width*2 + "px";
+			break;	
+	}
+})
