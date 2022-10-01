eb41db27239ff2393d4647dbafb4f1735cf15295
IlyaDevN <nevedrov.87@gmail.com>
2022-10-01 10:44:32 +0300

    slider js

--- js/script.js
+++ js/script.js
@@ -1,40 +1,10 @@
 'use strict'
-
-// ourVisitors__sliderCheckbox.addEventListener("click", function(event){
-// 	if(!event.target.closest("button") && event.target.tagName != "INPUT") return;
-// 	let photoContainerCoords = ourVisitors__photo_container.getBoundingClientRect();
-// 	let btn = event.target.closest("button");
-	
-// 	if(btn && btn.classList.contains("btn_prev")){
-// 		slider_range_ourVis.value--;
-// 		if(slider_range_ourVis.value <= 1) {
-// 			slider_range_ourVis.value = 1;
-// 		}
-// 	}
-// 	if(btn && btn.classList.contains("btn_next")){
-// 		slider_range_ourVis.value++;
-// 		if(slider_range_ourVis.value >= 3) {
-// 			slider_range_ourVis.value = 3;
-// 		}
-// 	}
-
-// 	switch (+slider_range_ourVis.value) {
-// 		case 1:
-// 			ourVisitors__photo_container.style.marginLeft = "0px";
-// 			break;
-// 		case 2:
-// 			ourVisitors__photo_container.style.marginLeft = -photoContainerCoords.width + "px";
-// 			break;
-// 		case 3:
-// 			ourVisitors__photo_container.style.marginLeft = -photoContainerCoords.width*2 + "px";
-// 			break;	
-// 	}
-// })
-
-let photoContainerCoords = ourVisitors__photo_container.getBoundingClientRect();
+/* ============================================================================
+				ourVisitors slider
+=============================================================================*/
 
 btn_prev_ourVis.addEventListener("click", function(){
-
+	
 	slider_range_ourVis.value--;
 	if(slider_range_ourVis.value <= 1) slider_range_ourVis.value = 1;
 	changeContainerPosition(slider_range_ourVis.value);
@@ -47,6 +17,8 @@ btn_next_ourVis.addEventListener("click", function(){
 })
 
 function changeContainerPosition(sliderRangeValue){
+	let photoContainerCoords = ourVisitors__photo_container.getBoundingClientRect();
+	
 	switch (+sliderRangeValue) {
 		case 1:
 			ourVisitors__photo_container.style.marginLeft = "0px";
