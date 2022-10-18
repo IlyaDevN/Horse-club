87b5f7f3a0536db77a94a361496e59f0e132f85a
IlyaDevN <nevedrov.87@gmail.com>
2022-10-18 16:14:32 +0300

    second slider

--- js/script.js
+++ js/script.js
@@ -49,3 +49,92 @@ sliderThumbCurrentPosition += sliderThumbWidth;
 	}
 	sliderThumb.style.marginLeft = sliderThumbCurrentPosition + "px";
 });
+
+/* ============================================================================
+				ourHorses slider SWIPER
+=============================================================================*/
+
+let swiper = new Swiper(".swiper-container", {
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev"
+	},
+	scrollbar: {
+		el: ".swiper-scrollbar",
+		draggable: true
+	},
+	navigation: {
+		nextEl: '.swiper-next',
+		prevEl: '.swiper-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'progressbar',
+	},
+	autoplay: {
+		delay: 1000,
+		reverseDirection: false
+	},
+	speed: 1000,
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			slidesPerGroup: 1,
+			spaceBetween: 10,
+			speed: 500,
+		},
+		480: {
+			slidesPerView: 2,
+			spaceBetween: 10,
+			slidesPerGroup: 2,
+		}, 
+		768: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+			slidesPerGroup: 1,
+			speed: 1000,
+		},
+		1920: {
+			slidesPerView: 5,
+			spaceBetween: 30,
+			slidesPerGroup: 1
+		}
+	},
+});
+
+
+let viewportWidth = window.innerWidth;
+if(viewportWidth >= 1920){
+
+	let slideCollection = galaryRow.querySelectorAll(".galary__item");
+	let currentSecondSlide = slideCollection[swiper.realIndex+1];
+	let currentThirdSlide = slideCollection[swiper.realIndex+2];
+	let currentFourthSlide = slideCollection[swiper.realIndex+3];
+	currentSecondSlide.classList.add("extremeSlides");
+	currentThirdSlide.classList.add("extremeSlides");
+	currentFourthSlide.classList.add("extremeSlides");
+
+	swiper.on("slideChange", function(){
+		let secondSlide = slideCollection[swiper.realIndex+1];
+		let thirdSlide = slideCollection[swiper.realIndex+2];
+		let fourthSlide = slideCollection[swiper.realIndex+3];
+	
+		currentSecondSlide.classList.remove("extremeSlides");
+		currentThirdSlide.classList.remove("extremeSlides");
+		currentFourthSlide.classList.remove("extremeSlides");
+	
+		secondSlide.classList.add("extremeSlides");
+		thirdSlide.classList.add("extremeSlides");
+		fourthSlide.classList.add("extremeSlides");
+	
+		currentSecondSlide = secondSlide;
+		currentThirdSlide = thirdSlide;
+		currentFourthSlide = fourthSlide;
+	});
+	swiper.on("reachEnd", function(){
+		swiper.reverseDirection = !swiper.reverseDirection;
+		swiper.update();
+	})
+}
+
+
