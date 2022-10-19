a09a2043607e958e15022cba0414c9760c876e2d
IlyaDevN <nevedrov.87@gmail.com>
2022-10-19 22:04:13 +0300

    coments deleted

--- js/script.js
+++ js/script.js
@@ -3,51 +3,64 @@
 /* ============================================================================
 				ourVisitors slider
 =============================================================================*/
-//SLIDER CONTAINER Variables
-let slidesQuantity = ourVisitors__photo_container.querySelectorAll(".ourVisitors__photo").length;
-let slideWidth = ourVisitors__photo_container.querySelector(".ourVisitors__photo").offsetWidth;
+
+const slidesContainer = document.getElementById("ourVisitors__photo_container");
+const slidesQuantity = slidesContainer.querySelectorAll(".ourVisitors__photo").length;
+const slideWidth = slidesContainer.querySelector(".ourVisitors__photo").offsetWidth;
+const sliderMinPossiblePosition = -(slidesQuantity - 1)*slideWidth;
+const sliderMaxPossiblePosition = 0;
 let sliderCurrentPosition = 0;
-let sliderMinPossiblePosition = -(slidesQuantity - 1)*slideWidth;
-let sliderMaxPossiblePosition = 0;
 
-//SLIDER RANGE Variables
-let sliderRangeWidth = sliderRange.offsetWidth;
-let sliderThumbWidth = sliderRangeWidth/slidesQuantity;
+const sliderRangeWidth = document.getElementById("slider_range").offsetWidth;
+const sliderThumb = document.getElementById("slider_thumb");
+const sliderThumbWidth = sliderRangeWidth/slidesQuantity;
+const sliderThumbMinPossiblePosition = 0;
+const sliderThumbMaxPossiblePosition = (slidesQuantity - 1)*sliderThumbWidth;
 let sliderThumbCurrentPosition = 0;
-let sliderThumbMinPossiblePosition = 0;
-let sliderThumbMaxPossiblePosition = (slidesQuantity - 1)*sliderThumbWidth;
 
-// Устанавливаем ширину ползунка в зависимости от количества слайдов
+const btnPrevOurVisitors = document.getElementById("btn_prev_ourVisitors");
+const btnNextOurVisitors = document.getElementById("btn_next_ourVisitors");
+
 sliderThumb.style.width = sliderThumbWidth + "px"; 
 
 btnPrevOurVisitors.addEventListener("click", function(){
-//СМЕЩЕНИЕ СЛАЙДЕРА
-	sliderCurrentPosition += slideWidth;
-	if(sliderCurrentPosition >= sliderMaxPossiblePosition){
-		sliderCurrentPosition = sliderMaxPossiblePosition;
-	}
-	ourVisitors__photo_container.style.marginLeft = sliderCurrentPosition + "px";
-//СМЕЩЕНИЕ ПОЛЗУНКА
-	sliderThumbCurrentPosition -= sliderThumbWidth;
-	if(sliderThumbCurrentPosition <= sliderThumbMinPossiblePosition){
-		sliderThumbCurrentPosition = sliderThumbMinPossiblePosition;
-	}
-	sliderThumb.style.marginLeft = sliderThumbCurrentPosition + "px";
+
+	(function sliderMove(){
+		sliderCurrentPosition += slideWidth;
+		if(sliderCurrentPosition >= sliderMaxPossiblePosition){
+			sliderCurrentPosition = sliderMaxPossiblePosition;
+		}
+		ourVisitors__photo_container.style.marginLeft = sliderCurrentPosition + "px";
+	})();
+	
+	(function thumbMove(){
+		sliderThumbCurrentPosition -= sliderThumbWidth;
+		if(sliderThumbCurrentPosition <= sliderThumbMinPossiblePosition){
+			sliderThumbCurrentPosition = sliderThumbMinPossiblePosition;
+		}
+		sliderThumb.style.marginLeft = sliderThumbCurrentPosition + "px";
+	})();
+	
 });
 
 btnNextOurVisitors.addEventListener("click", function(){
-//СМЕЩЕНИЕ СЛАЙДЕРА
-	sliderCurrentPosition -= slideWidth;
-	if(sliderCurrentPosition <= sliderMinPossiblePosition){
-		sliderCurrentPosition = sliderMinPossiblePosition;
-	}
-	ourVisitors__photo_container.style.marginLeft = sliderCurrentPosition + "px";
-//СМЕЩЕНИЕ ПОЛЗУНКА
-sliderThumbCurrentPosition += sliderThumbWidth;
-	if(sliderThumbCurrentPosition >= sliderThumbMaxPossiblePosition){
-		sliderThumbCurrentPosition = sliderThumbMaxPossiblePosition;
-	}
-	sliderThumb.style.marginLeft = sliderThumbCurrentPosition + "px";
+
+	(function sliderMove(){
+		sliderCurrentPosition -= slideWidth;
+		if(sliderCurrentPosition <= sliderMinPossiblePosition){
+			sliderCurrentPosition = sliderMinPossiblePosition;
+		}
+		ourVisitors__photo_container.style.marginLeft = sliderCurrentPosition + "px";
+	})();
+	
+	(function thumbMove(){
+		sliderThumbCurrentPosition += sliderThumbWidth;
+		if(sliderThumbCurrentPosition >= sliderThumbMaxPossiblePosition){
+			sliderThumbCurrentPosition = sliderThumbMaxPossiblePosition;
+		}
+		sliderThumb.style.marginLeft = sliderThumbCurrentPosition + "px";
+	})();
+
 });
 
 /* ============================================================================
@@ -96,7 +109,8 @@ let swiper = new Swiper(".swiper-container", {
 });
 
 let viewportWidth = window.innerWidth;
-if(viewportWidth >= 1920){
+const screenResolution = 1920;
+if(viewportWidth >= screenResolution){
 
 	let slideCollection = galaryRow.querySelectorAll(".galary__item");
 	let currentSecondSlide = slideCollection[swiper.realIndex+1];
