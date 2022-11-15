66c5df6ef3ac73ca0b7e0501d508a01e4f64dd1d
IlyaDevN <nevedrov.87@gmail.com>
2022-11-15 18:52:43 +0200

    made modal window and menu burger in intro

--- js/intro.js
+++ js/intro.js
@@ -1,8 +1,22 @@
-let burgerBtn = document.getElementById("menu_burger");
-let headerContainer = document.getElementById("header_container");
+const burgerBtn = document.getElementById("menu_burger");
+const nav = document.getElementById("nav__menu_header");
+const movingFrame = document.getElementById("moving_frame");
+const modalCloseBtn = document.getElementById("modal_close_btn");
+const modal = document.getElementById("modal");
 
-burgerBtn.addEventListener("click", function(){
+if(window.innerWidth < 768){
+	
+	burgerBtn.addEventListener("click", function(){
 
-	headerContainer.classList.add("active");
+		modal.classList.add("modal_showed");
+		document.body.classList.add("body_modal");
+		
+		movingFrame.append(nav);
+	});
 
-})
+	modalCloseBtn.addEventListener("click", function(){
+
+		modal.classList.remove("modal_showed");
+		document.body.classList.remove("body_modal");
+	})
+}
