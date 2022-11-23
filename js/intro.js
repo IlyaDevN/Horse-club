78cc1778d15687aa42051c75b4920fda8ff76dbb
IlyaDevN <nevedrov.87@gmail.com>
2022-11-23 09:19:19 +0200

    renamed variable in intro js

--- js/intro.js
+++ js/intro.js
@@ -1,29 +1,33 @@
-const burgerBtn = document.getElementById("menu_burger");
-const nav = document.getElementById("nav__menu_header");
-const movingFrame = document.getElementById("moving_frame");
-const modalCloseBtn = document.getElementById("modal_close_btn");
-const modal = document.getElementById("modal");
-const currentInnerWidth = 768;
+(function burger (){
 
-modal.style.transform = `translateX(-${window.innerWidth}px)`;
+	const burgerBtn = document.getElementById("menu_burger");
+	const nav = document.getElementById("nav__menu_header");
+	const movingFrame = document.getElementById("moving_frame");
+	const modalCloseBtn = document.getElementById("modal_close_btn");
+	const modal = document.getElementById("modal");
+	const tabletInnerWidth = 768;
 
-window.addEventListener("resize", function(){
 	modal.style.transform = `translateX(-${window.innerWidth}px)`;
-})
 
-if(window.innerWidth < currentInnerWidth){
-	
-	burgerBtn.addEventListener("click", function(){
-
-		modal.classList.add("modal_showed");
-		document.body.classList.add("body_modal");
-		modal.style.transform = `translateX(0)`;
-	});
-
-	modalCloseBtn.addEventListener("click", function(){
-
-		modal.classList.remove("modal_showed");
-		document.body.classList.remove("body_modal");
+	window.addEventListener("resize", function(){
 		modal.style.transform = `translateX(-${window.innerWidth}px)`;
 	})
-}
+
+	if(window.innerWidth < tabletInnerWidth){
+		
+		burgerBtn.addEventListener("click", function(){
+
+			modal.classList.add("modal_showed");
+			document.body.classList.add("body_modal");
+			modal.style.transform = `translateX(0)`;
+		});
+
+		modalCloseBtn.addEventListener("click", function(){
+
+			modal.classList.remove("modal_showed");
+			document.body.classList.remove("body_modal");
+			modal.style.transform = `translateX(-${window.innerWidth}px)`;
+		})
+	}
+
+})()
