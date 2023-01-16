92614d82e981f0a0625a309798ca2354a70b609e
Ilya Nevedrov <nevedrov.87@gmail.com>
2023-01-16 20:24:45 +0200

    "clientsSpeakAboutUs" logic completely remade

--- js/clientsSpeakAboutUs.js
+++ js/clientsSpeakAboutUs.js
@@ -10,44 +10,32 @@
 			el: ".swiper_scrollbar_2",
 			draggable: true,
 		},
-		// autoplay: {
-		// 	delay: 2500,
-		// 	disableOnInteraction: false,
-		// 	pauseOnMouseEnter: true,
-		// 	reverseDirection: false,
-		// },
 		breakpoints: {
 			320: {
 				slidesPerView: 1,
 				slidesPerGroup: 1,
 				spaceBetween: 10,
 			},
-			480: {
-				slidesPerView: 1,
-				slidesPerGroup: 1,
-				spaceBetween: 10,
-			}, 
 			768: {
 				slidesPerView: 2,
-				slidesPerGroup: 1,
 				spaceBetween: 10,
 			},
 			1920: {
 				slidesPerView: 4,
 				spaceBetween: 15,
-				slidesPerGroup: 1
 			}
 		},
 		speed: 1000,
 	})
-	
-	let viewportWidth = window.innerWidth;
-	const screenResolution = 1920;
 
 	changeButtonAppearance(swiperComments);
 	swiperComments.on("slideChange", changeButtonAppearance);
+	swiperComments.on("slideChange", closeComments);
 
-	if(viewportWidth >= screenResolution){
+	if(window.matchMedia("(min-width: 1000px)").matches){
+		swiperComments.allowTouchMove = false;
+	}
+	if(window.matchMedia("(min-width: 1920px)").matches){
 		for(let slide of swiperComments.slides){
 			slide.classList.add("activeSlides");
 		}
@@ -55,93 +43,76 @@
 		
 		swiperComments.on("slideChange", function(){
 			setSlidesState(swiperComments, "activeSlides");
-			closeComments();
 		});
 	}
-	// swiperComments.on("reachBeginning", function(){
-	// 	swiperComments.params.autoplay.reverseDirection = !swiperComments.params.autoplay.reverseDirection;
-	// });
-	// swiperComments.on("reachEnd", function(){
-	// 	swiperComments.params.autoplay.reverseDirection = !swiperComments.params.autoplay.reverseDirection;
-	// })
 })();
 
-
 (function unwrapCommentsButtons(){
-	
-	const btns = document.querySelectorAll(".card__detailed_container");
 
-	for(let btn of btns){
-		btn.addEventListener("click", function(){
-			closeComments();
-			openComment(btn);
+	const slides = document.querySelectorAll(".feedbacks__card_container");
+
+	slides.forEach(slide => {
+		const cardBtn = slide.querySelector(".card__unwrap_btn");
+
+		cardBtn.addEventListener("click", function(){
+			openComment(slide);
 		})
-	}
-
-	function openComment(button){
-		const comment = button.previousElementSibling;
-		const commentHeight = defineNodeHeight(comment);
-
-		if(!comment.classList.contains("open")){
-			document.documentElement.style.setProperty('--height', `${commentHeight}px`);
-			comment.classList.add("open");
-			button.classList.add("open");
-		}
-	}
+	});
 })()
 
-function closeComments(){
-	const comments = document.querySelectorAll(".card__text.open");
-	if(comments.length === 0) return;
+function openComment(slide){
+	const cardText = slide.querySelector(".card__text");
+	const cardBtn = slide.querySelector(".card__unwrap_btn");
+	const cardTextHeight = cardText.scrollHeight;
 
-	for(let comment of comments){
+	closeComments(slide);
 
-		setTimeout(() => {
-			comment.classList.remove("open");
-			document.documentElement.style.setProperty('--height', "");
-			comment.nextElementSibling.classList.remove("open");
-		}, 0);
+	if(!cardText.classList.contains("open")){
+		document.documentElement.style.setProperty('--cardText-height', `${cardTextHeight}px`);
+		cardText.classList.add("open");
+		cardBtn.classList.add("open");
+	} 
+	else {
+		closeComments();
 	}
+}
+
+function closeComments(slideNotToClose){
+
+	const slides = document.querySelectorAll(".feedbacks__card_container");
+	if(slides.length === 0) return;
+
+	slides.forEach(slide => {
+		if(slide === slideNotToClose) return;
+		
+		const cardText = slide.querySelector(".card__text.open");
+		const cardBtn = slide.querySelector(".card__unwrap_btn.open");
+
+		if(!cardText || !cardBtn) return;
+
+		cardText.classList.remove("open");
+		cardBtn.classList.remove("open");
+
+	});
 }
 
 (function changeUnwrapBtnVisibility(){
 	window.addEventListener("load", function(){
-
 		const timeout = 1000;
 
 		setTimeout(()=>{
 
-			const textContainers = document.querySelectorAll(".card__text");
-			const resolution = window.innerWidth;
-			
-			if(textContainers.length == 0) return;
+			const slides = document.querySelectorAll(".feedbacks__card_container");
+			if(slides.length === 0) return;
 
-			for(let container of textContainers){
+			slides.forEach(slide => {
+				const cardText = slide.querySelector(".card__text");
+				const cardBtn = slide.querySelector(".card__unwrap_btn");
+				if(cardText.scrollHeight > cardText.clientHeight){
+					cardBtn.classList.add("calculated");
+				}
+			});
 
-				const textNode = container.firstChild;
-				const textNodeHeight = defineNodeHeight(textNode);
-				const lineHeightInPx = getComputedStyle(container).lineHeight;
-				const lineHeight = lineHeightInPx.replace(/px/ig, "");
-				const quantityOfLines = Math.ceil(textNodeHeight / lineHeight);
-				
-				if(resolution < 480 && quantityOfLines > 5){
-					container.nextElementSibling.classList.add("calculated");
-				}
-				if(resolution >= 480 && resolution < 1000 && quantityOfLines > 4){
-					container.nextElementSibling.classList.add("calculated");
-				}
-				if(resolution >= 1000 && quantityOfLines > 3){
-					container.nextElementSibling.classList.add("calculated");
-				}
-			}
 		}, timeout);
 	})
 })()
-
-function defineNodeHeight(textNode){
-
-	const range = document.createRange();
-	range.selectNodeContents(textNode);
-	const textNodeHeight = range.getBoundingClientRect().height;
-	return textNodeHeight + 5;
-}
