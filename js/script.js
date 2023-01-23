d829d94d6e8a222fab2ef70593a1018df22e8461
Ilya Nevedrov <nevedrov.87@gmail.com>
2023-01-23 22:35:39 +0200

    Module swiperGlobalFunctionality was created

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ handleBurgerButtons();
 import popupRegistration from "./modules/popupRegistration.js";
 popupRegistration();
 
+import { setSlidesState } from "./modules/swiperGlobalFunctionality.js";
+import { getActiveIndexes } from "./modules/swiperGlobalFunctionality.js";
+import { navigationButtonAppearance } from "./modules/swiperGlobalFunctionality.js";
