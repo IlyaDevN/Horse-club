cfc54ff51ea609d34429305d12e9c6b5c11db0f9
Ilya Nevedrov <nevedrov.87@gmail.com>
2023-01-23 23:04:33 +0200

    Module swiperOurHorsesFunctionality.js was created

--- js/script.js
+++ js/script.js
@@ -4,6 +4,5 @@ handleBurgerButtons();
 import popupRegistration from "./modules/popupRegistration.js";
 popupRegistration();
 
-import { setSlidesState } from "./modules/swiperGlobalFunctionality.js";
-import { getActiveIndexes } from "./modules/swiperGlobalFunctionality.js";
-import { navigationButtonAppearance } from "./modules/swiperGlobalFunctionality.js";
+import initializeOurHorsesSlider from "./modules/swiperOurHorsesFunctionality.js";
+initializeOurHorsesSlider();
