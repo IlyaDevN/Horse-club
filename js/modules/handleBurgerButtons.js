d718b1c2069ad13af7b92b8b3b5d70650c86aa1a
Ilya Nevedrov <nevedrov.87@gmail.com>
2023-02-02 15:33:33 +0200

    refactor: Remove menu_body from js in menu burger

--- js/modules/handleBurgerButtons.js
+++ js/modules/handleBurgerButtons.js
@@ -1,13 +1,11 @@
 const menuOverlay = document.body.querySelector(".menu_overlay");
 const burgerBtn = document.body.querySelector(".menu_burger");
 const menuCloseBtn = menuOverlay.querySelector(".menu_close_btn");
-const menuBody = menuOverlay.querySelector(".menu_body");
 
 burgerBtn.addEventListener("click", openMenu);
 
 function openMenu(){
 	menuOverlay.classList.add("active");
-	menuBody.classList.add("active");
 	document.body.classList.add("stopPageScroll");
 	document.addEventListener("click", closeMenu);
 }
@@ -15,7 +13,6 @@ function openMenu(){
 function closeMenu(event){
 	if(event.target === menuOverlay || event.target.offsetParent === menuCloseBtn){
 		menuOverlay.classList.remove("active");
-		menuBody.classList.remove("active");
 		document.body.classList.remove("stopPageScroll");
 		document.removeEventListener("click", closeMenu);
 	}
