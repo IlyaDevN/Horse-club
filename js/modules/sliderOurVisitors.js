ffcee0b35ee7b2f8a7279a2a1f85de9bbcb7a24f
Ilya Nevedrov <nevedrov.87@gmail.com>
2023-02-03 13:49:21 +0200

    refactor: Remake sliderOurVisitors

--- js/modules/sliderOurVisitors.js
+++ js/modules/sliderOurVisitors.js
@@ -1,65 +1,119 @@
 const slidesContainer = document.getElementById("ourVisitors__photo_container");
 const slidesQuantity = slidesContainer.querySelectorAll(".ourVisitors__photo").length;
-const slideWidth = slidesContainer.querySelector(".ourVisitors__photo").offsetWidth;
-const sliderMinPossiblePosition = -(slidesQuantity - 1)*slideWidth;
+let slideWidth = slidesContainer.querySelector(".ourVisitors__photo").offsetWidth;
+let sliderMinPossiblePosition = -(slidesQuantity - 1)*slideWidth;
 const SLIDER_MAX_POSSIBLE_POSITION = 0;
-let sliderCurrentPosition = 0;
+let sliderCurrentShift = 0;
+let sliderCurrentPosition;
 
 const sliderRangeWidth = document.getElementById("slider_range").offsetWidth;
 const sliderThumb = document.getElementById("slider_thumb");
 const sliderThumbWidth = sliderRangeWidth/slidesQuantity;
 const SLIDER_THUMB_MIN_POSSIBLE_POSITION = 0;
 const sliderThumbMaxPossiblePosition = (slidesQuantity - 1)*sliderThumbWidth;
-let sliderThumbCurrentPosition = 0;
+let sliderThumbCurrentShift = 0;
 
 const btnPrevOurVisitors = document.getElementById("btn_prev_ourVisitors");
 const btnNextOurVisitors = document.getElementById("btn_next_ourVisitors");
 
 sliderThumb.style.width = sliderThumbWidth + "px"; 
 
+const mql320 = window.matchMedia("(min-width: 320px)");
+const mql480 = window.matchMedia("(min-width: 480px)");
+const mql768 = window.matchMedia("(min-width: 768px)");
+const mql1000 = window.matchMedia("(min-width: 1000px)");
+const mql1920 = window.matchMedia("(min-width: 1920px)");
+
+const mqlArray = [mql320, mql480, mql768, mql1000, mql1920];
+
+mqlArray.forEach(mql => {
+	mql.addEventListener("change", setState);
+});
+
 btnPrevOurVisitors.addEventListener("click", function(){
-
-	sliderCurrentPosition += slideWidth;
-	if(sliderCurrentPosition >= SLIDER_MAX_POSSIBLE_POSITION){
-		sliderCurrentPosition = SLIDER_MAX_POSSIBLE_POSITION;
-		this.disabled = true;
-	} else{
-		this.disabled = false;
-	}
-	
+	movePrevSlide();
+	moveThumbPrev();
 	if(btnNextOurVisitors.disabled){
-		btnNextOurVisitors.disabled = false;
+		enableButton(btnNextOurVisitors);
 	}
-
-	slidesContainer.style.transform = "translateX(" + sliderCurrentPosition + "px)";
-
-	sliderThumbCurrentPosition -= sliderThumbWidth;
-	if(sliderThumbCurrentPosition <= SLIDER_THUMB_MIN_POSSIBLE_POSITION){
-		sliderThumbCurrentPosition = SLIDER_THUMB_MIN_POSSIBLE_POSITION;
-	}
-	sliderThumb.style.transform = "translateX(" + sliderThumbCurrentPosition + "px)";
 });
 
 btnNextOurVisitors.addEventListener("click", function(){
-
-	sliderCurrentPosition -= slideWidth;
-	if(sliderCurrentPosition <= sliderMinPossiblePosition){
-		sliderCurrentPosition = sliderMinPossiblePosition;
-		this.disabled = true;
-	}
-	else{
-		this.disabled = false;
-	}
+	moveNextSlide();
+	moveThumbNext();
 
 	if(btnPrevOurVisitors.disabled){
-		btnPrevOurVisitors.disabled = false;
+		enableButton(btnPrevOurVisitors);
 	}
-	
-	slidesContainer.style.transform = "translateX(" + sliderCurrentPosition + "px)";
-
-	sliderThumbCurrentPosition += sliderThumbWidth;
-	if(sliderThumbCurrentPosition >= sliderThumbMaxPossiblePosition){
-		sliderThumbCurrentPosition = sliderThumbMaxPossiblePosition;
-	}
-	sliderThumb.style.transform = "translateX(" + sliderThumbCurrentPosition + "px)";
 });
+
+function movePrevSlide(){
+	sliderCurrentShift += slideWidth;
+
+	if(sliderCurrentShift >= SLIDER_MAX_POSSIBLE_POSITION){
+		sliderCurrentShift = SLIDER_MAX_POSSIBLE_POSITION;
+		disableButton(btnPrevOurVisitors);
+	} else{
+		enableButton(btnPrevOurVisitors);
+	}
+	slidesContainer.style.transform = `translateX(${sliderCurrentShift}px)`;
+}
+
+function moveNextSlide(){
+	sliderCurrentShift -= slideWidth;
+	if(sliderCurrentShift <= sliderMinPossiblePosition){
+		sliderCurrentShift = sliderMinPossiblePosition;
+		disableButton(btnNextOurVisitors);
+	}
+	else{
+		enableButton(btnNextOurVisitors);
+	}
+	slidesContainer.style.transform = `translateX(${sliderCurrentShift}px)`;
+}
+
+function moveThumbPrev(){
+	sliderThumbCurrentShift -= sliderThumbWidth;
+	if(sliderThumbCurrentShift <= SLIDER_THUMB_MIN_POSSIBLE_POSITION){
+		sliderThumbCurrentShift = SLIDER_THUMB_MIN_POSSIBLE_POSITION;
+	}
+	sliderThumb.style.transform = `translateX(${sliderThumbCurrentShift}px)`;
+}
+
+function moveThumbNext(){
+	sliderThumbCurrentShift += sliderThumbWidth;
+	if(sliderThumbCurrentShift >= sliderThumbMaxPossiblePosition){
+		sliderThumbCurrentShift = sliderThumbMaxPossiblePosition;
+	}
+	sliderThumb.style.transform = `translateX(${sliderThumbCurrentShift}px)`;
+}
+
+function enableButton(button){
+	button.disabled = false;
+}
+
+function disableButton(button){
+	button.disabled = true;
+}
+
+function defineState(){
+	sliderCurrentPosition = sliderCurrentShift/slideWidth;
+	
+	slideWidth = slidesContainer.querySelector(".ourVisitors__photo").offsetWidth;
+	sliderMinPossiblePosition = -(slidesQuantity - 1)*slideWidth;
+	sliderCurrentShift = slideWidth * sliderCurrentPosition;
+	sliderThumbCurrentShift = -sliderThumbWidth * sliderCurrentPosition;
+	
+	const slideState = {
+
+		sliderShift: sliderCurrentShift,
+		sliderThumbShift: sliderThumbCurrentShift
+	};
+	
+	return slideState;
+}
+
+function setState(){
+	const sliderState = defineState();
+	slidesContainer.style.transform = "translateX(" + sliderState.sliderShift + "px)";
+	sliderThumb.style.transform = "translateX(" + sliderState.sliderThumbShift + "px)";
+}
