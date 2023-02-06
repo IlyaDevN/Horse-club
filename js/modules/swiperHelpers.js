982756de6e2e9dab63749feebaffa7433c67f822
Ilya Nevedrov <nevedrov.87@gmail.com>
2023-02-06 14:07:40 +0200

    refactor: Use parameters instead fo this

--- js/modules/swiperHelpers.js
+++ js/modules/swiperHelpers.js
@@ -1,6 +1,6 @@
 export function setSlidesState(){
 	const swiper = this;
-	const activeIndexes = getActiveIndexes.call(swiper);
+	const activeIndexes = getActiveIndexes(swiper);
 	const slideCollection = swiper.slides;
 	const mqlOver1920 = window.matchMedia("(min-width: 1920px)");
 
@@ -22,34 +22,32 @@ export function setSlidesState(){
 			swiper.on("slideChange", setSlidesState);
 		} else {
 			swiper.off("slideChange", setSlidesState);
-			cleanSlidesState.call(swiper);
+			cleanSlidesState(swiper);
 		}
 	}
 }
 
-function cleanSlidesState(){
-	const swiper = this;
-	swiper.slides.forEach(slide => {
+function cleanSlidesState(slider){
+	slider.slides.forEach(slide => {
 		slide.classList.remove("activeSlides");
 	});
 }
 
-function getActiveIndexes(){
-	const swiper = this;
+function getActiveIndexes(slider){
 	let activeIndexes = [];
-	let activeIndex = swiper.realIndex;
-	if(swiper.realIndex == 0){
-		for(let i = 0; i < swiper.params.slidesPerView; i++){
+	let activeIndex = slider.realIndex;
+	if(slider.realIndex == 0){
+		for(let i = 0; i < slider.params.slidesPerView; i++){
 			activeIndexes.push(i);
 		}
 	}
-	if(swiper.realIndex == swiper.slides.length - swiper.params.slidesPerView){
-		for(let i = swiper.realIndex; i < swiper.slides.length; i++){
+	if(slider.realIndex == slider.slides.length - slider.params.slidesPerView){
+		for(let i = slider.realIndex; i < slider.slides.length; i++){
 			activeIndexes.push(i);
 		}
 	}
 	else {
-		const quantityOfActiveIndexes = swiper.params.slidesPerView - 2;
+		const quantityOfActiveIndexes = slider.params.slidesPerView - 2;
 		for(let i = 0; i < quantityOfActiveIndexes; i++){
 			activeIndexes.push(++activeIndex);
 		}
