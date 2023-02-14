da4f59345fbed4afdd8c39e2ee23a7a1cb3b818e
Ilya Nevedrov <nevedrov.87@gmail.com>
2023-02-14 18:27:46 +0200

    refactor: Create separate module mediaQueries.js

--- js/modules/helpers.js
+++ js/modules/helpers.js
@@ -27,11 +27,3 @@ export function throttle(func, ms){
 
 	return wrapper;
 }
-
-const mql320 = window.matchMedia("(min-width: 320px)");
-const mql480 = window.matchMedia("(min-width: 480px)");
-const mql768 = window.matchMedia("(min-width: 768px)");
-const mql1000 = window.matchMedia("(min-width: 1000px)");
-const mql1920 = window.matchMedia("(min-width: 1920px)");
-
-export const mqlArray = [mql320, mql480, mql768, mql1000, mql1920];
---
+++ js/modules/mediaQueries.js
@@ -0,0 +1,7 @@
+const mql320 = window.matchMedia("(min-width: 320px)");
+const mql480 = window.matchMedia("(min-width: 480px)");
+const mql768 = window.matchMedia("(min-width: 768px)");
+const mql1000 = window.matchMedia("(min-width: 1000px)");
+const mql1920 = window.matchMedia("(min-width: 1920px)");
+
+export const mqlArray = [mql320, mql480, mql768, mql1000, mql1920];
