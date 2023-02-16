140a1b2a22bdeef00a343eb207be04627bf9fb01
Ilya Nevedrov <nevedrov.87@gmail.com>
2023-02-16 11:01:49 +0200

    refactor: Modify inputValidation using RULES

--- js/modules/inputValidation.js
+++ js/modules/inputValidation.js
@@ -1,111 +1,110 @@
 const buttons = document.querySelectorAll("button[type = submit]");
-const VALIDATE = {
-	onSubmit: false,
+let onSubmit = false;
+let isInputHandlerAdded = false;
 
-	name: function (input){
-		const regExp = /^[а-яА-ЯёЁa-zA-ZЁёЇїІіЄєҐґ']+$/
-		const str = input.value;
+const RULES = [
 
-		if(this.onSubmit){
-			if(!regExp.test(str)){
-				return false;
-			}
-		} else {
-			if(!input.value){
-				return true;
-			}
-			if(!regExp.test(str)){
-				return false;
-			}
-		}
-		return true;
-	},
+	{
+		name: "name",
+		validate: function(input){
+			const regExp = /^[а-яА-ЯёЁa-zA-ZЁёЇїІіЄєҐґ']+$/
+			const str = input.value;
 
-	phone: function (input){
-		const regExp = /^(?! )^[+\-\s()0-9]{1,20}/
-		const str = input.value;
-
-		if(this.onSubmit){
-			if(!regExp.test(str) || input.value.length < 10){
-				return false;
-			}
-		} else {
-			if(!input.value){
-				return true;
-			}
-			if(!regExp.test(str)){
-				return false;
+			if(onSubmit){
+				return regExp.test(str);
+			} else {
+				if(!input.value){
+					return true;
+				}
+				return regExp.test(str);
 			}
 		}
-		return true;
 	},
+	{
+		name: "phone",
+		validate: function(input){
+			const regExp = /^(?! )^[+\-\s()0-9]{1,20}$/
+			const str = input.value;
 
-	checkbox: function (input){
-
-		if(this.onSubmit){
-			if(!input.checked){
-				return false;
+			if(onSubmit){
+				return regExp.test(str) && input.value.length >= 10;
+			} else {
+				if(!input.value){
+					return true;
+				}
+				return regExp.test(str);
 			}
 		}
-		return true;
 	},
-
-}
-
-Object.defineProperty(VALIDATE, "onSubmit", {enumerable: false});
-
-function validateHandler(button){
-	
-	const form = button.closest("form");
-	const inputCollection = form.querySelectorAll("input");
-	const validateKeys = Object.keys(VALIDATE);
-	let isPermitted = true;
-	let map = new Map();
-
-	inputCollection.forEach((input)=>{
-
-		validateKeys.forEach((key) => {
-
-			if(input.hasAttribute(`data-${key}`)) {
-				VALIDATE.onSubmit = true;
-				const isValid = VALIDATE[key](input);
-				map.set(input, isValid);
-				addInputHandler(input, VALIDATE[key]);
+	{
+		name: "checkbox",
+		validate: function(input){
+			if(onSubmit){
+				return input.checked;
 			}
-		})
-	});
-
-	for(let entry of map){
-		const input = entry[0];
-		const isInputValid = entry[1];
-
-		if(!isInputValid){
-			showError(input);
-			isPermitted = false;
+			return true;
 		}
 	}
-	return isPermitted;
+]
+
+function validateField(input){
+	let isValid;
+
+	RULES.forEach((rule) => {
+		if(Object.hasOwn(input.dataset, rule.name)){
+			isValid = rule.validate(input);
+		}
+	});
+	return isValid;
+}
+
+function isFormValid(form){
+	let checkings = [];
+	
+	const filteredFields = Array.from(form.elements).filter((element)=>{
+		return Object.hasOwn(element.dataset, "toValidate");
+	});
+
+	filteredFields.forEach((element)=> {
+
+		const isValid = validateField(element);
+
+		if(!isInputHandlerAdded) {
+			addInputHandler(element);
+		}
+		
+		checkings.push(isValid);
+
+		if(!isValid) {
+			showError(element);
+		} else {
+			hideError(element);
+		}
+	})
+	isInputHandlerAdded = true;
+	
+	return checkings.every((item) => {
+		return item;
+	})
 }
 
 buttons.forEach((button) => {
 	
-	button.addEventListener("click", (event)=>{
-		event.preventDefault();
-		const isValid = validateHandler(button);
-		const form = button.closest("form");
+	button.addEventListener("click", () => {
+		onSubmit = true;
+		const isValid = isFormValid(button.form);
 		
 		if(isValid) {
-			formSubmit(form);
+			formSubmit(button.form);
 		}
 	})
 });
 
-function addInputHandler(input, validFunc){
+function addInputHandler(input){
 
 	input.addEventListener("input", ()=>{
-		
-		VALIDATE.onSubmit = false;
-		const isValid = validFunc.call(VALIDATE, input);
+		onSubmit = false;
+		const isValid = validateField(input);
 
 		hideError(input);
 
@@ -119,7 +118,7 @@ function formSubmit(form){
 	let submit = new Event("submit");
 	form.dispatchEvent(submit);
 	// form.submit();
-	clearInput(form);
+	form.reset();
 }
 
 function showError(input){
@@ -130,18 +129,4 @@ function showError(input){
 function hideError(input){
 	const inputContainer = input.closest(".input_container");
 	inputContainer.classList.remove("error");
-}
-
-function clearInput(form){
-	const inputsToClear = form.querySelectorAll("input");
-
-	inputsToClear.forEach((input) => {
-
-		if(input.type === "checkbox") {
-			input.checked = false;
-		}
-		else {
-			input.value = "";
-		}
-	})
 }
