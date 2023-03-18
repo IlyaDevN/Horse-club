249ebe62b10a85bd606191f8b074733080f60f33
Ilya Nevedrov <nevedrov.87@gmail.com>
2023-03-18 10:53:49 +0200

    refactor: Modify burger button using span

--- js/modules/mobileMenu.js
+++ js/modules/mobileMenu.js
@@ -1,23 +1,23 @@
 const menuOverlay = document.body.querySelector(".menu_overlay");
-const burgerBtn = document.body.querySelector(".menu_burger");
+const burgerBtn = document.body.querySelector(".menu-burger-button");
 const menuCloseBtn = menuOverlay.querySelector(".menu_close_button");
 
 burgerBtn.addEventListener("click", openMenu);
 menuOverlay.addEventListener("click", closeMenuByOverlay);
 menuCloseBtn.addEventListener("click", closeMenu);
 
-function openMenu(){
+function openMenu() {
 	menuOverlay.classList.add("active");
 	document.body.classList.add("stopPageScroll");
 }
 
-function closeMenuByOverlay(event){
-	if(event.target === menuOverlay) {
+function closeMenuByOverlay(event) {
+	if (event.target === menuOverlay) {
 		closeMenu();
 	}
 }
 
-function closeMenu(){
+function closeMenu() {
 	menuOverlay.classList.remove("active");
 	document.body.classList.remove("stopPageScroll");
 }
