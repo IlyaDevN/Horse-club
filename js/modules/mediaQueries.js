41bbfd4fc749ea29c4902a5ce6a6a86143542880
Ilya Nevedrov <nevedrov.87@gmail.com>
2023-03-19 09:13:45 +0200

    refactor: Remove useless media queries

--- js/modules/mediaQueries.js
+++ js/modules/mediaQueries.js
@@ -1,6 +1,1 @@
-export const mql320 = window.matchMedia("(min-width: 320px)");
-export const mql480 = window.matchMedia("(min-width: 480px)");
-export const mql768 = window.matchMedia("(min-width: 768px)");
-export const mql1000 = window.matchMedia("(min-width: 1000px)");
 export const mql1920 = window.matchMedia("(min-width: 1920px)");
-export const mqlArray = [mql320, mql480, mql768, mql1000, mql1920];
