f39b53fe4161763bfa25f5fe15063cf957737ebb
Ilya Nevedrov <nevedrov.87@gmail.com>
2023-04-26 00:09:45 +0300

    refactor: Remove converting of fonts

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
-import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
+import { copyFonts } from "./gulp/tasks/fonts.js";
 
 //наблюдатель за измемениями в файлах
 function watcher() {
@@ -35,11 +35,8 @@ function watcher() {
 	gulp.watch(path.watch.images, images);
 }
 
-//Последовательная обработка шрифтов
-const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
-
 //Основные задачи
-const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
+const mainTasks = gulp.series(copyFonts, gulp.parallel(copy, html, scss, js, images));
 
 //построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
