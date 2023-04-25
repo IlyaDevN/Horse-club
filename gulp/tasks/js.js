feaed33f9b3ce753f8177c15caceb2c5236b7540
Ilya Nevedrov <nevedrov.87@gmail.com>
2023-04-25 22:53:04 +0300

    refactor: Rename index.js to index.min.js

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -11,8 +11,7 @@ export const js = () => {
 		.pipe(webpack({
 			mode: app.isBuild ? "production" : "development",
 			output: {
-				// filename: "app.min.js",
-				filename: "index.js",
+				filename: "index.min.js",
 			}
 		}))
 		.pipe(app.gulp.dest(app.path.build.js))
