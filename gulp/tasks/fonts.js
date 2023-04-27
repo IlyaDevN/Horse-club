58a9b22a75dd46a28dd1df5fac3921f71a1ad39a
Ilya Nevedrov <nevedrov.87@gmail.com>
2023-04-27 17:20:34 +0300

    rafactor: Modify gulp task fonts.js

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,10 +1,4 @@
 export const copyFonts = () => {
-			//Searching for fonts .woff
-			return app.gulp.src(`${app.path.srcFolder}/fonts/*.woff`)
-			//Move to folder with results
-			.pipe(app.gulp.dest(`${app.path.build.fonts}`))
-			//Searching for fonts .woff2
-			.pipe(app.gulp.src(`${app.path.srcFolder}/fonts/*.woff2`))
-			//Move to folder with results
-			.pipe(app.gulp.dest(`${app.path.build.fonts}`))
+		return app.gulp.src(app.path.src.fonts)
+			.pipe(app.gulp.dest(app.path.build.fonts))
 }
