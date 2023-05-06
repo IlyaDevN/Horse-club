a28b12475d4013d8d7209815711d3494c76154b4
Ilya Nevedrov <nevedrov.87@gmail.com>
2023-05-06 17:43:13 +0300

    refactor: Remove "gulp-newer" from plugins

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,14 +1,12 @@
 import plumber from "gulp-plumber"; // handling of errors
 import notify from "gulp-notify"; // tooltip messages
 import browsersync from "browser-sync"; //local server
-import newer from "gulp-newer"; //checking of update
 import ifPlugin from "gulp-if"; //conditional branching
 
 export const plugins = {
 	plumber: plumber,
 	notify: notify,
 	browsersync: browsersync,
-	newer: newer,
 	if: ifPlugin
 }
 
