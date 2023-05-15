c7c305175957532e548c34361ba54721ae689b2f
IlyaDevN <nevedrov.87@gmail.com>
2023-05-15 18:29:13 +0300

    refactor: Rename inputValidation.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,5 @@ import "./modules/popupRegistration.js";
 import "./modules/swiperOurHorsesFunctionality.js";
 import "./modules/swiperCommentsFunctionality.js";
 import "./modules/sliderOurVisitors.js";
-import "./modules/inputValidation.js";
+import "./modules/form-validation.js";
 import "./modules/mapFunctionality.js";
