7d8e3fd42cec1a2908d27adbde39e81ea3682c82
IlyaDevN <nevedrov.87@gmail.com>
2023-06-07 11:29:22 +0300

    refactor: Completely remake modal-box

--- src/js/modules/modal-box.js
+++ src/js/modules/modal-box.js
@@ -1,94 +1,78 @@
-const popupLinks = document.querySelectorAll(".popup-link");
-const modalOverlays = document.querySelectorAll(".modal-overlay");
+const modals = document.querySelectorAll(".modal-overlay");
+const modalGratitude = document.getElementById("modal-gratitude");
 const forms = Array.from(document.forms);
 const KEYCODE = {
 	ESC: "Escape"
 };
-let modalOverlay;
-let modalContent;
-let gratitude;
-let openButton;
+let activeModal = null;
 
-popupLinks.forEach((popupLink) => popupLink.addEventListener("click", openModal));
-forms.forEach((form) => form.addEventListener("submitSuccess", submitHandler));
-modalOverlays.forEach((overlay) => overlay.addEventListener("click", emptyPlaceCloseHandler));
+forms.forEach((form) => form.addEventListener("submitSuccess", openGratitude));
 
-function defineModal(event) {
-	if (event.target.tagName == "FORM") {
-		openButton = event.target.querySelector("button");
+modals.forEach((modal) => {
+	const openButtons = document.querySelectorAll(`[data-modal-link="${modal.id}"]`);
+	const closeButtons = modal.querySelectorAll(".modal-close-button");
+
+	openButtons.forEach((button) => button.addEventListener("click", () => openModal(modal)));
+	closeButtons.forEach((button) => button.addEventListener(("click"), () => closeModal(modal)));
+
+	modal.addEventListener("click", closeByOverlay);
+})
+
+function openModal(modal, isGratitude = false) {
+	modal.classList.add("open");
+
+	if(isGratitude) {
+		modal.querySelector(".gratitude").classList.add("open");
 	} else {
-		openButton = event.target.closest("button");
+		modal.querySelector(".modal-content").classList.add("open");
 	}
 
-	modalOverlay = document.getElementById(openButton.dataset.modalLink);
-	modalContent = modalOverlay.querySelector(".modal-content");
-	gratitude = modalOverlay.querySelector(".gratitude");
-	const closeButtons = modalOverlay.querySelectorAll(".modal-close-button");
-	closeButtons.forEach((button) => button.addEventListener("click", closeModal));
-}
-
-function openModal(event) {
-	defineModal(event);
-	openOverlay();
-	modalContent.classList.add("open");
-}
-
-function openOverlay() {
-	modalOverlay.classList.add("open");
+	activeModal = modal;
 	disablePageScroll();
-	document.addEventListener("keydown", keyDownHandler);
+	document.addEventListener("keydown", closeByEscape);
 }
 
-function openGratitude(gratitude) {
-	if(gratitude) {
-		gratitude.classList.add("open");
+function closeModal(modal) {
+	const content = modal.querySelector(".modal-content");
+	const gratitude = modal.querySelector(".gratitude");
+
+	if(content){
+		content.classList.remove("open");
 	}
-}
-
-function closeGratitude(gratitude) {
-	if(gratitude) {
+	if(gratitude){
 		gratitude.classList.remove("open");
 	}
+
+	modal.classList.remove("open");
+	document.removeEventListener("keydown", closeByEscape);
+	activeModal = null;
+	modal.addEventListener("transitionend", enablePageScroll, { once: true });
 }
 
-function emptyPlaceCloseHandler(event) {
-	if (event.target === modalOverlay) {
-		closeModal();
+function openGratitude() {
+	if(activeModal) {
+		activeModal.querySelector(".modal-content").classList.remove("open");
+		activeModal.querySelector(".gratitude").classList.add("open");
+	} else {
+		openModal(modalGratitude, true);
 	}
 }
 
-function keyDownHandler(event) {
+function closeByEscape(event){
 	if (event.code === KEYCODE.ESC) {
-		closeModal();
+		closeModal(activeModal);
 	}
 }
 
-function submitHandler(event) {
-	if (!modalOverlay) {
-		defineModal(event);
+function closeByOverlay(event){
+	const modal = this;
+	if(event.target === modal){
+		closeModal(modal);
 	}
-	if (modalContent.classList.contains("open")) {
-		modalContent.classList.remove("open");
-	}
-
-	if (!modalOverlay.classList.contains("open")) {
-		openOverlay();
-	}
-	openGratitude(gratitude);
-	document.addEventListener("keydown", keyDownHandler);
-}
-
-function closeModal() {
-	modalOverlay.classList.remove("open");
-	document.removeEventListener("keydown", keyDownHandler);
-	modalOverlay.addEventListener("transitionend", enablePageScroll, { once: true });
-	modalContent.classList.remove("open");
-	closeGratitude(gratitude);
 }
 
 function disablePageScroll() {
 	const scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
-
 	document.body.classList.add("stopPageScroll");
 	document.body.style.paddingRight = scrollBarWidth + "px";
 }
