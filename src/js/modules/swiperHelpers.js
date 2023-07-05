d24176907116b4f79df2715ea40136fb84a693e6
IlyaDevN <nevedrov.87@gmail.com>
2023-07-05 21:18:06 +0300

    refactor: Add indent to section before loading
    Add indent 200px to section before loading slider images

--- src/js/modules/swiperHelpers.js
+++ src/js/modules/swiperHelpers.js
@@ -42,14 +42,13 @@ export function loadAllSliderImages(sectionClass) {
 	const section = document.querySelector(sectionClass);
 
 	const observer = new IntersectionObserver(([entry], observer) => {
-		
 		if (entry.isIntersecting) {
 			const images = section.querySelectorAll("[loading='lazy']");
 
 			images.forEach((image)=> image.setAttribute("loading", "eager"));
 			observer.unobserve(entry.target);
 		}
-	});
+	}, {rootMargin: "0px 0px 200px"});
 
 	observer.observe(section);
 }
